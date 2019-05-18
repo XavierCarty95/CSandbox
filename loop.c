86c3d113970034c766eaa2afa207258594aea3d8
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-05-18 03:21:12 +0000

    c playground

---
+++ loop.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int main(){
+    int luckyNumbers[] = {4,8,15,16,23, 42};
+
+    int i;
+    for(i = 1; i <= 5; i++)
+    {
+        printf("%d\n",luckyNumbers[i]);
+    }
+
+}
