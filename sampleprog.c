96e9048dbc24024b43495e376b8de56cabcc4d59
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-06-12 23:46:48 -0400

    Update sampleprog.c

--- sampleprog.c
+++ sampleprog.c
@@ -1,1 +1,15 @@
 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+ float pmt,roi,sint;
+ int tm;
+ printf("Enter the principal amount, time and rate of interest");
+ scanf("%f%d%f", &pmt,&tm,&roi);
+ sint=(pmt * tm * roi)/100;
+ printf("Simple Interest = %.2f", sint);
+
+}
